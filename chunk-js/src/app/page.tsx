393026caa847f9c3ce551d76ad06760a32fbbc34
Beecher Baker <beecher@beechersoftware.com>
2024-12-16 18:55:57 -0500

    show diff between original and chunked text

--- chunk-js/src/app/page.tsx
+++ chunk-js/src/app/page.tsx
@@ -4,6 +4,7 @@ import { experimental_useObject as useObject } from "ai/react"
 import { useCallback, useState } from "react"
 import { ChunksSchema } from "./chunksSchema"
 import { GLEANINGS_I } from "./gleaningsOne"
+import { diffWords } from "diff"
 
 export default function Home() {
     const [document, setDocument] = useState(GLEANINGS_I)
@@ -56,6 +57,23 @@ export default function Home() {
                     </ol>
                 </>
             )}
+            <Diff a={document} b={chunks.join(" ")} />
+        </div>
+    )
+}
+
+const Diff = ({a, b}: {a: string, b: string}) => {
+    const changes = diffWords(a, b)
+    return (
+        <div className="w-[80vw] sm:max-w-[40rem]">
+            <h2 className="text-xl">Changes:</h2>
+            <ul>
+                {changes.map((change, i) => (
+                    <li key={i} className={change.added ? "text-green-600" : change.removed ? "text-red-600" : "text-gray-400"}>
+                        {change.value}
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
