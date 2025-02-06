a4ca75c557142caa99af0a8b2d984230d5dce188
Beecher Baker <beecher@beechersoftware.com>
2025-02-06 17:38:26 -0500

    remove dead comment

--- web/src/app/api/document/extract/route.ts
+++ web/src/app/api/document/extract/route.ts
@@ -4,29 +4,6 @@ import { makeStepStreamer } from "@/lib/makeStepStreamer"
 // it can take a long time to download a document
 export const maxDuration = 300
 
-// export async function POST(req: Request) {
-//     const { documentUrl } = await req.json()
-//     // based on https://youtu.be/_Rb4SpWRHC8?si=rirB2yxL9HNOtqae&t=900
-//     const stream = new ReadableStream({
-//         async start(controller) {
-//             const [setStep, stepsDone, steps] = makeSetStep(controller)
-//             await extractDocument(documentUrl, setStep)
-//             const lastStep = steps.length ? steps[steps.length - 1] : undefined
-//             console.log({ lastStep, documentUrl })
-//             stepsDone()
-//             controller.close()
-//         }
-//     })
-//
-//     return new Response(stream, {
-//         headers: {
-//             "Content-Type": "application/json",
-//             "Transfer-Encoding": "chunked",
-//         },
-//     })
-// }
-//
-
 export async function POST(req: Request) {
     const { documentUrl } = await req.json()
     if (typeof documentUrl !== "string")
--- web/src/lib/makeStepStreamer.ts
+++ web/src/lib/makeStepStreamer.ts
@@ -1,9 +1,10 @@
 import { makeSetStep, SetStep } from "@/lib/stepStatus"
 
-// Create a POST or GET that streams progress updates through its Response, and can be listened to with the AI SDK's experimental_useObject.
+// Create an API endpoint that streams progress updates through its Response, and can be listened to with the AI SDK's experimental_useObject.
 type steppedFunction<ParameterType> = (p: ParameterType, setStep: SetStep) => Promise<void>
 export const makeStepStreamer = <ParameterType>(f: steppedFunction<ParameterType>) => {
     return async (p: ParameterType) => {
+        // based on https://youtu.be/_Rb4SpWRHC8?si=rirB2yxL9HNOtqae&t=900
         const stream = new ReadableStream({
             async start(controller) {
                 // const [setStep, stepsDone] = makeSetStep(controller)
