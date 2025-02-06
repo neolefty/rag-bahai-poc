11f958fa08a08dfbee77fba82a1304ea9575c8ff
Beecher Baker <beecher@beechersoftware.com>
2025-02-05 22:42:23 -0500

    tweak debug printing; slight cleanup

--- web/src/app/_load/TransformDocument.ts
+++ web/src/app/_load/TransformDocument.ts
@@ -25,27 +25,14 @@ export const breakIntoBlocks = async (
 
     setStep("breaking into blocks")
     // split into blocks, using unified
-    // console.log(document.raw_html)
     const tree: Root = unified()
         .use(rehypeParse, {
             fragment: false,
             compressWhitespace: true,
         })
         .parse(`${document.raw_html}`)
-    // console.log({tree})
     debugPrint(tree)
-    // if (tree.children)
-    //     tree.children.forEach((node, i) => {
-    //         if (node.type === "text")
-    //             console.log({i, nodeType: node.type, value: node.value})
-    //         else if (node.type === "element")
-    //             console.log({i, nodeType: node.type, tagName: node.tagName})
-    //         else
-    //             console.log({i, nodeType: node.type})
-    //     })
 
-    // const body = tree.children.find(node => node.type === "element" && node.tagName === "body")
-    // if (!body) throw new Error(`No body found in document #${document.id} "${document.title}"`)
 
 
         // .use(rehypeStringify)
@@ -53,35 +40,39 @@ export const breakIntoBlocks = async (
         // .parse(document.raw_html)
 }
 
-const TEXT_MAX_LENGTH = 80
+const TEXT_MAX_WIDTH = 80
 
 const debugPrint = (node: Root | RootContent, depth = 0) => {
     const indent = "  ".repeat(depth)
     const shorten = (s: string) =>
-        s.length > TEXT_MAX_LENGTH-depth*2
-            ? s.slice(0, TEXT_MAX_LENGTH-3-depth*2) + "..."
+        s.length > TEXT_MAX_WIDTH-depth*2
+            ? s.slice(0, TEXT_MAX_WIDTH-3-depth*2) + "..."
             : s
+
     if (node.type === "text") {
         const text = node.value.trim()
-        if (text) {
-            console.log(`${indent}${node.type}: ${shorten(text)} (${node.value.length})`)
-        }
+        if (text) console.log(`${indent}${node.type}: ${shorten(text)} (${node.value.length})`)
     }
+
     else if (node.type === "element") {
         const id = node.properties?.id ? `#${node.properties.id}` : ""
+        let className = node.properties?.className
+        const classes = Array.isArray(className) ? className.join(".") : className
         const text = node.children && node.children.length === 1 && node.children[0].type === "text" ? node.children[0].value.trim() : undefined
         const shortText = text ? ` "${shorten(text)}" (${text.length})` : ""
-        console.log(`${indent}${node.tagName}${id}${shortText ? `: ${shortText}`: ""}`)
+        console.log(`${indent}${node.tagName}${id}${shortText ? `: ${shortText}`: ""}${classes ? `.${classes}` : ""}`)
         if (node.children && !shortText) {
             node.children.forEach(child => debugPrint(child, depth + 1))
         }
     }
+
     else if (node.type === "root") {
-        console.log(`${indent}${node.type}`)
         if (node.children) {
+            console.log(`${indent}${node.type}`)
             node.children.forEach(child => debugPrint(child, depth + 1))
         }
     }
+
     else {
         console.log(`${indent}${node.type}`)
     }
